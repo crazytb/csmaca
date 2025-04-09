f240c69d976ec587b4265ff4061107497ae5b0b3
Taewon Song (mac) <crazytb@gmail.com>
2025-04-09 21:24:52 +0900

    Refactor STA class: rename start_transmission to begin_transmission and update packet arrival handling

--- random_access.py
+++ random_access.py
@@ -15,22 +15,35 @@ SWITCHING_DELAY = 10    # 슬롯
 # PRIMARY_OCCUPANCY_PROB = 0.3
 
 class STA:
-    def __init__(self, id, use_npca):
+    def __init__(self, id, use_npca=False):
         self.id = id
         self.use_npca = use_npca
         self.cw = CW_MIN
-        self.backoff = np.random.randint(0, self.cw + 1)
-        self.in_npca = False
-        self.remaining_tx = 0
-        self.switching = 0
-        self.packet_arrival_time = 0
-        self.current_channel = "primary"  # 'primary' 또는 'npca'
         self.state = "idle" # "idle", "backoff", "transmitting", "on_difs"
-        self.is_retransmission = False
-        self.last_aoi = 0
+        self.backoff = 0
+        self.remaining_difs = 0
+        self.remaining_tx = 0
         self.ppdu_duration = 0
+        self.current_channel = "primary"  # 'primary' 또는 'npca'
+        self.primary_channel = None
+        self.npca_channel = None
+        self.last_packet_time = 0
         
-
+    def set_channel(self, channel):
+        # Channel: 1 or 2
+        assert channel in [1, 2], "Invalid channel number"
+        self.primary_channel = channel
+        self.npca_channel = 3-channel
+        
+    def generate_ppdu(self, current_time):
+        # PPDU 생성 로직
+        self.ppdu_duration = np.random.randint(*PPDU_DURATION_RANGE)
+        self.remaining_tx = self.ppdu_duration
+        self.backoff = np.random.randint(0, self.cw + 1)
+        self.remaining_difs = DIFS // SLOT_TIME
+        self.state = "on_difs"
+        self.last_packet_time = current_time
+        
     def reset_backoff(self, current_time, collision=False):
         if collision:
             self.cw = min((self.cw + 1) * 2 - 1, CW_MAX)
@@ -40,28 +53,19 @@ class STA:
         self.ppdu_duration = 0
         self.current_channel = "primary"
         
-    def set_channel(self, channel):
-        self.primary_channel = channel
-        self.npca_channel = 3-channel
+    
         
-    def generate_ppdu(self, current_time):
-        # PPDU 생성 로직
-        self.ppdu_duration = np.random.randint(*PPDU_DURATION_RANGE)
-        self.remaining_tx = self.ppdu_duration
-        self.backoff = np.random.randint(0, self.cw + 1)
-        self.packet_arrival_time = current_time
-        self.state = "on_difs"
-        self.waiting_slot = DIFS // SLOT_TIME
-        self.creation_time = current_time
+    
         
-    def start_transmission(self):
+        
+    def begin_transmission(self):
         assert self.state == "on_difs", "STA is not in DIFS state"
         assert self.remaining_tx == 0, "STA has remaining transmission time"
         if self.use_npca and self.in_npca:
         
             self.state = "transmitting"
             self.remaining_tx = self.ppdu_duration
-            self.last_aoi = self.creation_time + self.ppdu_duration
+            self.last_aoi = self.packet_arrival_time + self.ppdu_duration
         
 
 class Channel:
